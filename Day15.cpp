24315d321ea60cd21b6e57ea8a648c52c97911f8
manishregmi999 <manishregmi007@gmail.com>
2026-01-07 22:22:00 +0545

    Add files via upload

---
+++ Day15.cpp
@@ -0,0 +1,26 @@
+
+// Common Time Complexities
+
+// O(1) - Constant Time Complexity:
+// Its execution time does not depend on the size of the input.
+// For Example: Accessing an array by using an index.
+
+// O(log n) - Logarithmic Time
+// The size of the problems is systematically reduced. Example: Binary Search.
+
+// O(n) - Linear Time
+// The time of execution will increase with the size of the input. Example: Traversing an array once.
+
+// O(n²) — Quadratic Time
+// The time taken for execution increases with the square of the size of the input.
+// Example: Nested loops: Bubble sort.
+
+// O(n³) — Cubic Time
+// Often seen in three nested loop cases.
+// Example: Naive matrix multiplication.
+
+// Why Time Complexity Matters
+// As discussed in
+// Assists in writing scalable and efficient code
+// Enables the comparison of solutions
+// Prevents performance problems with large inputs
