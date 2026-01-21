618f26d750366644a2849999b36cdd49d6410074
manishregmi999 <manishregmi007@gmail.com>
2026-01-21 13:33:45 +0545

    Add files via upload

---
+++ Day27.cpp
@@ -0,0 +1,20 @@
+//Tree
+Root: Topmost node
+Parent: Node which connects to the child
+Child: Node which is conected by another node is its child
+Leaf/External node: Nodes with no child
+Internal Node: Nodes with tlease one child
+Depth: No of edges from root to the node
+Heigth: No. of edges from root to the deepest leaf.
+Siblings: Nodes belonging to the same parent 
+Ancestors: Parent or parent of parents or all higher node up to the root.
+Descendent: Children or childrens of children or all lower level nodes of a node.
+
+//Binary Tree
+Binary Tree: It is a tree which has at most 2 children for all the nodes.
+//Types of binary tree
+- Full or strict Binary Tree
+- Perfect binary tree
+- Complete Binary Tree 
+- Degenerate Tree
+- Skewed Tree
